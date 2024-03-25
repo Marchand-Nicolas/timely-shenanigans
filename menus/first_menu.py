dbd0a672fd747a05a86b3fe9cd532359977c5616
Quentin Godard <quentingodard09@gmail.com>
2024-03-25 14:53:38 +0100

    debut

--- menus/first_menu.py
+++ menus/first_menu.py
@@ -3,5 +3,9 @@ import pygame
 
 
 def show_first_menu(screen):
-    rect = pygame.Rect(screen.get_width(), screen.get_height(), 50, 100)
-    play_button = pygame.draw.rect(screen, (255, 45, 0), rect)
+    rect = pygame.Rect(screen.get_width(), 100, 50, 100)
+    run = True
+    while run:
+        
+        pygame.draw.rect(screen, (255, 255, 255), rect)
+        pygame.display.flip()
--- utils/create_screen.py
+++ utils/create_screen.py
@@ -5,6 +5,6 @@ def create_screen():
     pygame.init()  # initialisation du module "pygame"
 
     screen = pygame.display.set_mode(
-        (0, 0), pygame.FULLSCREEN
+        (800, 500)
     )  # Création d'une fenêtre graphique de taille 600x600 pixels
     return screen
