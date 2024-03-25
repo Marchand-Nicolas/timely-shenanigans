cdd7ce319a936930650670d7f91b8173dfbde70d
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-03-25 14:41:23 +0100

    test create_screen small changes

--- utils/create_screen.py
+++ utils/create_screen.py
@@ -1,10 +1,9 @@
 import pygame
 
-
 def create_screen():
     pygame.init()  # initialisation du module "pygame"
 
     screen = pygame.display.set_mode(
-        (0, 0), pygame.FULLSCREEN
+        (1000, 500) #pygame.FULLSCREEN
     )  # Création d'une fenêtre graphique de taille 600x600 pixels
     return screen
