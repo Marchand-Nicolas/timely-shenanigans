e8b889ada7b7a71b0f06f91296165f95e4012f0e
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-03-25 14:47:36 +0100

    create screen modif Elliot

--- utils/create_screen.py
+++ utils/create_screen.py
@@ -6,4 +6,11 @@ def create_screen():
     screen = pygame.display.set_mode(
         (1000, 500) #pygame.FULLSCREEN
     )  # Création d'une fenêtre graphique de taille 600x600 pixels
+    Running = True
+    while Running:
+        pygame.event.get()
+        pygame.display.update()
+        pygame.display.quit()
     return screen
+
+create_screen()
