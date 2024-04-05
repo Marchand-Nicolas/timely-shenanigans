5981db4a40e084921954f6c3b82d4641a8530953
Nico <nicomarchand29@gmail.com>
2024-04-05 09:54:17 +0200

    feat: game loop event handlers

--- src/test/world_generation.py
+++ src/test/world_generation.py
@@ -4,6 +4,7 @@ from src.map.renderer import render
 from src.utils.game_loop import game_loop
 from src.map.world import World
 from src.map.coordinates import Coordinates
+from src.utils.game_context import GameContext
 
 screen = create_screen()
 generated_assets = generate_map(123456789, 2000, 2000, 2)
@@ -12,6 +13,15 @@ world = World(2000, 2000, generated_assets, [(1000, 1000)])
 
 player_coordinates = Coordinates(200, 1000)
 
-render(world, player_coordinates, screen)
 
-game_loop()
+def refresh():
+    render(world, player_coordinates, screen)
+
+
+def process_event(event):
+    print(event)
+
+
+context = GameContext(refresh, process_event)
+
+game_loop(context)
--- src/utils/game_loop.py
+++ src/utils/game_loop.py
@@ -1,9 +1,21 @@
+# Nico
+
 import pygame
+from src.utils.game_context import GameContext
 
 
-def game_loop():
+def game_loop(context=None):
+    if context is None:
+        context = GameContext()
     run = True
     while run:
         for event in pygame.event.get():
+            if context.get_on_event():
+                # On appelle la fonction on_event à chaque événement
+                context.get_on_event()(event)
             if event.type == pygame.QUIT:
                 run = False
+
+        if context.get_tick_event():
+            # On appelle la fonction tick_event à chaque tour de boucle
+            context.tick_event()
