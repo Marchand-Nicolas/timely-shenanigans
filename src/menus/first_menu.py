f61ecf19ef8b0694dd91c3d4d19407961a089b81
Quentin Godard <quentingodard09@gmail.com>
2024-04-05 09:54:18 +0200

    affichage bouton pour lensser le jeu mais ne le fait pas disparetre quand on clic dessu

--- src/menus/first_menu.py
+++ src/menus/first_menu.py
@@ -1,9 +1,34 @@
 # quentin
 import pygame
 
+arial24 = pygame.font.SysFont("arial",24)
 
 def show_first_menu(screen):
-    run = True
-    while run:    
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(100, 100, 50, 100))        
-        pygame.display.flip()
+    title_game = arial24.render("titre du jeu",True,pygame.Color(255,255,255))
+    play_buton = pygame.Rect(screen.get_width()//2 - 50, screen.get_height()//2 - 50, 100, 100)
+    running = True
+    list = [play_buton]
+    while running:
+        #screen.blit(title_game, (screen.get_width()//2 - 50, screen.get_height()//2 - 100))
+        #pygame.draw.rect(screen, (255, 0, 0), play_buton)        
+        #pygame.display.flip()
+        display(list, screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos() 
+                if play_buton.collidepoint(x, y): 
+                    list = []
+                    display(list, screen)
+                    print("p")
+                    
+def display(list, screen):
+    if list == []:
+        pygame.display.update()
+    else:
+        for elt in list:
+            pygame.draw.rect(screen, (255, 0, 0), elt)
+            pygame.display.flip()
+
+                    
