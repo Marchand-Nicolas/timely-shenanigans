8d5421aebb61186ff3df0234e5f820ac7e0d20ae
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-04-22 14:38:48 +0200

    déplacements

--- src/utils/game_context.py
+++ src/utils/game_context.py
@@ -2,7 +2,7 @@ class GameContext:
     def __init__(self, tick_event=None, on_event=None):
         self.tick_event = tick_event
         self.on_event = on_event
-
+        
     def get_tick_event(self):
         return self.tick_event
 
