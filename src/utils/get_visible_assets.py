a18c1a8a0f931f0f0516662137d03b88873ef2c9
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-04-30 20:05:11 +0200

    improving visible assets getter

--- src/utils/get_visible_assets.py
+++ src/utils/get_visible_assets.py
@@ -6,30 +6,35 @@ from .screen import Screen
 def get_visible_assets(coordinates: Coordinates, assets: list, screen: Screen):
     visible_assets = []
 
-    # Précalcul des coordonnées de l'écran
-    screen_left = coordinates.get_x() - (screen.get_width() // 2)
-    screen_right = coordinates.get_x() + (screen.get_width() // 2)
-    screen_top = coordinates.get_y() - (screen.get_height() // 2)
-    screen_bottom = coordinates.get_y() + (screen.get_height() // 2)
+    # Précalcul
+    screen_x = coordinates.get_x()
+    screen_y = coordinates.get_y()
+    half_screen_width = screen.get_width() // 2
+    half_screen_height = screen.get_height() // 2
+
+    screen_left = screen_x - half_screen_width
+    screen_right = screen_x + half_screen_width
+    screen_top = screen_y - half_screen_height
+    screen_bottom = screen_y + half_screen_height
 
     for asset in assets:
-        (x, y) = asset.get_position()
-        image_width = asset.get_width()
-        image_height = asset.get_height()
+        x, y = asset.get_position()
+        half_image_width = asset.get_width() // 2
+        full_image_height = asset.get_height()
 
-        left_edge = x - image_width // 2
-        right_edge = x + image_width // 2
-        top_edge = y - image_height
-        bottom_edge = y + image_height
-
+        left_edge = x - half_image_width
+        right_edge = x + half_image_width
+        top_edge = y - full_image_height
+        bottom_edge = y + full_image_height
         if (
-            screen_left < right_edge
+            right_edge > screen_left
             and left_edge < screen_right
-            and screen_top < bottom_edge
+            and bottom_edge > screen_top
             and top_edge < screen_bottom
         ):
             visible_assets.append(asset)
 
+        # Les assets sont ordonnés par ordre croissant de y + height donc on peut arrêter la boucle dès qu'on dépasse le bas de l'écran
         if top_edge > screen_bottom:
             break
     return visible_assets
