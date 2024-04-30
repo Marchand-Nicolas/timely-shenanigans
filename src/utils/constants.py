8be3ce76d5b2c18590937b4b3bf3028a21e2d2f8
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2024-04-30 19:08:40 +0200

    further opti

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,6 +2,6 @@
 # Constantes du jeu
 server_address = "158.101.204.28"
 server_port = 3011
-world_width = 2000
-world_height = 2000
+world_width = 10000
+world_height = 10000
 speed = 200
--- src/utils/get_visible_assets.py
+++ src/utils/get_visible_assets.py
@@ -5,21 +5,22 @@ from .screen import Screen
 
 def get_visible_assets(coordinates: Coordinates, assets: list, screen: Screen):
     visible_assets = []
+
+    # Precompute screen boundaries
+    screen_left = coordinates.get_x() - (screen.get_width() // 2)
+    screen_right = coordinates.get_x() + (screen.get_width() // 2)
+    screen_top = coordinates.get_y() - (screen.get_height() // 2)
+    screen_bottom = coordinates.get_y() + (screen.get_height() // 2)
+
     for asset in assets:
         (x, y) = asset.get_position()
-        image = asset.get_image()
-        image_width = image.get_width()
-        image_height = image.get_height()
+        image_width = asset.get_width()
+        image_height = asset.get_height()
 
         left_edge = x - image_width // 2
         right_edge = x + image_width // 2
         top_edge = y - image_height
         bottom_edge = y + image_height
-
-        screen_left = coordinates.get_x() - (screen.get_width() // 2)
-        screen_right = coordinates.get_x() + (screen.get_width() // 2)
-        screen_top = coordinates.get_y() - (screen.get_height() // 2)
-        screen_bottom = coordinates.get_y() + (screen.get_height() // 2)
 
         if (
             screen_left < right_edge
