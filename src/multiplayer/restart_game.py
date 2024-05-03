34a7aa9616d5de97348690ca3ef4b92e662ce295
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-05-03 09:50:12 +0200

    fix restaart game route

--- src/multiplayer/restart_game.py
+++ src/multiplayer/restart_game.py
@@ -4,4 +4,4 @@ from src.multiplayer.call_server import call_server
 
 
 def restart_game(code):
-    return restart_game("restart_game", {"code": code})
+    return call_server("restart_game", {"code": code})
