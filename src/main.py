b41ade09b1a4b2595ad3c2562c91d8afdefdeeff
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-05-04 11:12:40 +0200

    commentaires pour rendre

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 
+""" 
+Pour lancer le jeu, entrez:
+python -m src.main
+dans la console
+
+pour tester la génération et les déplacements sans multi:
+python -m src.test.world_generation
+
+Si le serveur n'est pas allumé, vous pouvez aller dans src/utiils/constants.py et mettre l'ip à localhost:port pour vous connecter au serveur local
+et pour le lancer : python -m server.server
+"""
+
 import pygame
 from src.utils.create_screen import create_screen
 from src.menus.first_menu import show_first_menu
