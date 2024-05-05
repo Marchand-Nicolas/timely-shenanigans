ad94ec37e6fcccb971e62c41ec686ce54da1870b
Nico <nicomarchand29@gmail.com>
2024-05-05 14:10:54 +0200

    inncreasing game duration

--- src/utils/get_game_duration.py
+++ src/utils/get_game_duration.py
@@ -3,4 +3,4 @@ from src.utils.constants import game_duration_seconds
 
 
 def get_game_duration(player_amount):
-    return game_duration_seconds * player_amount
+    return 30 + game_duration_seconds * player_amount
