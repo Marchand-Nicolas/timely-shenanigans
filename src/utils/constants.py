89b244082fcd95d1c6aa7f6bc99db3d7e04745ee
Nico <nicomarchand29@gmail.com>
2024-05-05 14:00:41 +0200

    reducing map size

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,8 +2,8 @@
 # Constantes du jeu
 server_address = "158.101.204.28"
 server_port = 3011
-world_width = 4000
-world_height = 4000
+world_width = 2500
+world_height = 2500
 speed = 200
 game_duration_seconds = 40
 hunter_speed_multiplier = 1.5
