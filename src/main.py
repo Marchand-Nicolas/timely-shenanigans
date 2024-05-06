dfd6cd125f941339d591a3c4e662f7e023f64edf
Elliot Isken--Andersen Nexö <122434767+Elliot-Isken-Andersen-Nexo@users.noreply.github.com>
2024-05-06 13:18:22 +0200

    timely shenanigans

--- src/main.py
+++ src/main.py
@@ -17,6 +17,6 @@ from src.utils.create_screen import create_screen
 from src.menus.first_menu import show_first_menu
 
 screen = create_screen()  # Création d'une fenêtre graphique
-pygame.display.set_caption("Space Invader")  # Définit le titre de la fenêtre
+pygame.display.set_caption("Timely Shenanigans")  # Définit le titre de la fenêtre
 
 show_first_menu(screen)
