f4c9c40e2c1e812803b28202a18ec6d79c11cc24
Nico <nicomarchand29@gmail.com>
2024-05-06 13:15:15 +0200

    reducing game duration

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,6 +5,6 @@ server_port = 3011
 world_width = 2500
 world_height = 2500
 speed = 200
-game_duration_seconds = 40
+game_duration_seconds = 30
 hunter_speed_multiplier = 1.5
 ghost_speed_multiplier = 2
